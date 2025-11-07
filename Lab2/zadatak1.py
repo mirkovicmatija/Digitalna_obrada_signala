3c81c9d544ad410574aab88b0eb2f6e2410bd2c7
mirkovicmatija <matija.mirkovic003@gmail.com>
2025-11-07 14:23:42 +0100

    svi zadaci sa auditornih vjezbi do temina 11.11  uradjeni

--- Lab2/zadatak1.py
+++ Lab2/zadatak1.py
@@ -1,9 +1,45 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
 n = np.arange(32)
 x = np.cos(2 * np.pi * n / np.sqrt(31))
+y = np.fft.fft(x)
+y_512 = np.fft.fft(x, 512) 
 
+b = np.blackman(32)
+y_b = np.fft.fft(b, 512)
+x_b = b * x
+y_x_b = np.fft.fft(x_b,512)
+
+h = np.hamming(32)
+y_h = np.fft.fft(h, 512)
+x_h = h * x
+y_x_h = np.fft.fft(x_h,512)
+
+
+fig, [(graph0, graph1), (graph2, graph3), (graph4, graph5)] = plt.subplots(nrows = 3, ncols = 2)
+graph0.plot(n/16,x)
+graph1.stem(n/16,x)
+graph2.stem(n/16,np.abs(y))
+graph3.semilogx(n/16,np.abs(y))
+graph4.plot(np.arange(512),np.abs(y_512))
+graph5.semilogx(np.arange(512),np.abs(y_512))
+plt.show()
+
+fig2, [(graph_0, graph_1), (graph_2, graph_3), (graph_4, graph_5), (graph_6, graph_7), (graph_8, graph_9)] = plt.subplots(nrows = 5, ncols = 2)
+graph_0.plot(n/16,b)
+graph_1.plot(n/16,h)
+graph_2.plot(np.arange(512),np.abs(y_b))
+graph_3.plot(np.arange(512),np.abs(y_h))
+graph_4.semilogx(np.arange(512),np.abs(y_b))
+graph_5.semilogx(np.arange(512),np.abs(y_h))
+graph_6.plot(n/16,x_b)
+graph_7.plot(n/16,x_h)
+graph_8.plot(np.arange(512),np.abs(y_x_b))
+graph_9.plot(np.arange(512),np.abs(y_x_h))
+plt.show()
 """
+Matlab kod
 clc
 n = 0:31;
 x = cos(2*pi*n/sqrt(31));
@@ -15,7 +51,7 @@ y = abs(fft(x));
 
 w = blackman(32);
 f = w .* x';
-subplot(221),plot(0:31,w)
+subplot(221),plot(0:31,w)git pull
 subplot(222),plot(0:31,f)
 v = abs(fft(f,512));
 h = 0:511;
--- Lab3/zadatak1.py
+++ Lab3/zadatak1.py
@@ -1,22 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def function(tt , f):
+def function(tt):
+    f = 8000
     t = np.arange(tt)
     return np.cos(np.pi * 2 * 800 * t / f) + np.cos(np.pi * 2 * 888.8 * t / f) + np.cos(np.pi * 2 * 1600 * t / f)
 
-for i in range(3):
+funcs = [function, np.blackman, np.hamming]
+
+for f in funcs:
     fig, [(graph1, graph2), (graph3, graph4), (graph5, graph6), (graph7, graph8), (graph9, grapha)] = plt.subplots(nrows = 5, ncols = 2)
 
-    graph1.stem(np.arange(64),function(64,8000))
-    graph2.plot(np.arange(64),np.abs(np.fft.fft(function(64,8000),64)))
-    graph3.plot(np.arange(1024),np.abs(np.fft.fft(function(64,8000),1024)))
-    graph4.plot(np.arange(2048),np.abs(np.fft.fft(function(64,8000),2048)))
-    graph5.plot(np.arange(256),np.abs(np.fft.fft(function(256,8000))))
-    graph6.plot(np.arange(512),np.abs(np.fft.fft(function(512,8000))))
-    graph7.plot(np.arange(1024),np.abs(np.fft.fft(function(256,8000),1024)))
-    graph8.plot(np.arange(2048),np.abs(np.fft.fft(function(256,8000),2048)))
-    graph9.plot(np.arange(1024),np.abs(np.fft.fft(function(512,8000),1024)))
-    grapha.plot(np.arange(2048),np.abs(np.fft.fft(function(512,8000),2048)))
+    graph1.stem(np.arange(64),f(64))
+    graph2.plot(np.arange(64),np.abs(np.fft.fft(f(64),64)))
+    graph3.plot(np.arange(1024),np.abs(np.fft.fft(f(64),1024)))
+    graph4.plot(np.arange(2048),np.abs(np.fft.fft(f(64),2048)))
+    graph5.plot(np.arange(256),np.abs(np.fft.fft(f(256))))
+    graph6.plot(np.arange(512),np.abs(np.fft.fft(f(512))))
+    graph7.plot(np.arange(1024),np.abs(np.fft.fft(f(256),1024)))
+    graph8.plot(np.arange(2048),np.abs(np.fft.fft(f(256),2048)))
+    graph9.plot(np.arange(1024),np.abs(np.fft.fft(f(512),1024)))
+    grapha.plot(np.arange(2048),np.abs(np.fft.fft(f(512),2048)))
 
     plt.show()
