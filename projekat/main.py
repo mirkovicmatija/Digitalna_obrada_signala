22b8a1c8a283fb5cef22d9e39e2c4205a5d20f7b
mirkovicmatija <matija.mirkovic003@gmail.com>
2026-01-18 18:30:22 +0100

    zavrsni push  projekta

--- projekat/main.py
+++ projekat/main.py
@@ -9,13 +9,16 @@ import analyse as analyse
 
 #stereo -> mono 
 samplerate, data = preprocess.load_audio_mono('test_audio/13-10-22 shotgun.wav')
+samplerate_2, data_2 = preprocess.load_audio_mono('test_audio/13-10-22 shotgun 2.wav')
 
 #data spliting
 data_array = preprocess.split_audio(data, samplerate, segment_duration=3)
-
+data_array_2 = preprocess.split_audio(data_2, samplerate_2, segment_duration=3)
 #applyiing window function and resampling to 16kHz
 filtered = preprocess.preprocess_segments(data_array, samplerate, target_samplerate=16000)
+filtered_2 = preprocess.preprocess_segments(data_array_2, samplerate_2, target_samplerate=16000)
 
+#feature extraction and z-score normalization
 z_array = []
 for f in filtered:
     # Compute Mel-spectrogram
@@ -24,9 +27,18 @@ for f in filtered:
     z_array.append(z)
     #analyse.plot_spectrogram(log_mel_spectrogram)
 
-print(len(z_array))
+for f in filtered_2:
+    # Compute Mel-spectrogram
+    log_mel_spectrogram = analyse.analyze_segments(f, samplerate=16000)
+    z = analyse.zscore_normalization(log_mel_spectrogram)
+    z_array.append(z)
+    #analyse.plot_spectrogram(log_mel_spectrogram)
+
+#labels for training data
 train_data = np.array(z_array)
-label = np.array([1,1,1,1,0,1,0,0,0,1,0,0,1,1,0,1,0,1,0,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,0,0,1,0,0,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,0,1,1,0])
+label = np.array([1,1,1,1,0,1,0,0,0,1,0,0,1,1,0,1,0,1,0,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,
+                  1,1,0,0,1,0,0,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,
+                  1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,0,1,0])
 
 #reshaping data for training
 X = preprocess.reshape_segment(train_data)
